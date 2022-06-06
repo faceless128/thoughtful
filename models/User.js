96f65d356c1e1940cc3fd9e91dbc9ef8a563ca72
Kaijam Jeffers <faceless128@gmail.com>
2022-06-06 18:49:44 -0400

    work on user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            lowercase: true,
+            required: true,
+            validate: {
+                validator: function(v) {
+                  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+            trim: true,
+            unique: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [UserSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+)
+
+// get total count of comments and replies on retrieval
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length + 1;
+});
+
+// create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+// export the User model
+module.exports = User;
